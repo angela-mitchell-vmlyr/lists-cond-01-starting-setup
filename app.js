cf100033014d6e248b04b6936da4a3b1152b3586
Angela Mitchell <angela.mitchell@vmlyr.com>
2021-10-03 15:34:22 -0500

    learn new ways to use v-for

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       enteredGoalValue: '',
-      goals: [] 
+      goals: [],
+      person: { name: 'Angela', age: 32 }
     };
   },
   methods: {
